6ba50616d7229407fab39bd632400647176bbc24
razin <abyad.the.lord@gmail.com>
2026-09-10 03:09:37 +1000

    Add abstract parent class Actor using encapsulation

---
+++ src/sim/Actor.java
@@ -0,0 +1,75 @@
+package sim;
+
+import java.awt.Graphics;
+
+public abstract class Actor implements Drawable {
+
+    private static final int Emax = 200;
+    private static final int Emin = 0;
+
+    protected final World world;
+    private int x;
+    private int y;
+    private int energy;
+
+    public Actor(World world, int x, int y, int startingEnergy) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.energy = startingEnergy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public boolean isAlive() {
+        return energy > 0;
+    }
+
+    protected void changeEnergy(int E) {
+        energy += E;
+        if (energy > Emax) {
+            energy = Emax;
+        }
+        if (energy < Emin) {
+            energy = Emin;
+        }
+    }
+
+    protected void moveTo(int newX, int newY) {
+        if (newX < 0) {
+            newX = 0;
+        }
+        if (newX >= world.getWidth()) {
+            newX = world.getWidth() - 1;
+        }
+        if (newY < 0) {
+            newY = 0;
+        }
+        if (newY >= world.getHeight()) {
+            newY = world.getHeight() - 1;
+        }
+        x = newX;
+        y = newY;
+    }
+
+    protected void wander() {
+        int dx = world.getRandom().nextInt(3) - 1;
+        int dy = world.getRandom().nextInt(3) - 1;
+        moveTo(x + dx, y + dy);
+    }
+
+    public abstract void update();
+
+    @Override
+    public abstract void draw(Graphics g);
+}
